03d8d3883455b2e6b4d0531ed372e339dbe792e7
Angelina Nazarova <angelina@rerotor.ru>
2021-01-20 20:02:00 +0300

    Added tests with negative checks to test_product_page

--- pages/product_page.py
+++ pages/product_page.py
@@ -19,3 +19,10 @@ class ProductPage(BasePage):
         product_price = self.browser.find_element(*ProductPageLocators.PRICE).text
         message_price = self.browser.find_element(*ProductPageLocators.MESSAGE_CART_PRICE).text
         assert product_price == message_price, f"{product_price} not equal {message_price}"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGE_ADD_TO_BASKET), \
+            "Success message is presented, but should not be"
+    def should_disapper_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.MESSAGE_ADD_TO_BASKET), \
+            "Success message persists"
--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,11 @@
 import pytest
+
+from conftest import browser
 from pages.product_page import ProductPage
 
 
 @pytest.mark.parametrize("link", ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6", pytest.param("offer7", marks=pytest.mark.xfail), "offer8", "offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
-    browser.delete_all_cookies()
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={link}"
     page = ProductPage(browser, link)
     page.open()
@@ -12,3 +13,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.matching_name()
     page.matching_price()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_busket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_busket()
+    page.should_disapper_success_message()
