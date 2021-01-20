19deeea73e754b0cdd45a4801a51fb52f4edec0b
Angelina Nazarova <angelina@rerotor.ru>
2021-01-20 12:46:31 +0300

    Added login page assert tests

---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from .locators import MainPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+        login_url = self.browser.current_url
+        assert "login" in login_url, "URL does not contain 'login'"
+
+    def should_be_login_form(self):
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
